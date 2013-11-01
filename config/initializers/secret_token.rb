0ae32e0f62a287c1dee49120f8ad410488362dc2
Jacqui <jacqui@wishart.me>
2013-11-01 06:15:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onemonthrails::Application.config.secret_key_base = '214a400445a238aea92fee19f0750edbcc2fa0fa7cf21e94d453db942ffadc07ee18be7d3249c1e26c8e188867cb5619bd67a8cd8145cf043c0877d87a655e69'
