af8c6f00e83c6eeeef80059e62383bc19bf4032e
Jacqui Wishart <jacqui@wishart.me>
2013-11-06 20:06:05 -0600

    added columns to database for user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106213344) do
+ActiveRecord::Schema.define(version: 20131107015206) do
 
   create_table "skills", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "skills", ["user_id"], name: "index_skills_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
